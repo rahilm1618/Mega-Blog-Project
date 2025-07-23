da8db1cc968b6a062759db73ef2a8fc666226d55
rahilm1618 <164912474+rahilm1618@users.noreply.github.com>
2025-07-23 12:36:38 +0530

    Blog-Project

---
+++ Mega-Blog-Project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+    <div>hello</div>
+    </>
+  )
+}
+
+export default App
